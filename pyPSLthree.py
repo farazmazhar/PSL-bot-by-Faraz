f1282de05d4c1e759984efa2557519ac2334ae6b
Faraz Mazhar <farazmazhar@users.noreply.github.com>
2018-02-24 18:01:17 +0500

    Introducing discord api.

--- pyPSLthree.py
+++ pyPSLthree.py
@@ -4,11 +4,12 @@ Created on Sat Feb 24 12:45:14 2018
 
 @author: faraz
 """
-
+from disco.bot import Bot, Plugin
 import standings
 
-html_link = "http://www.espncricinfo.com/table/series/8679/season/2018/pakistan-sl"
-
-pslStandings = standings.Standing(html_link)
-
-print(pslStandings.getStandings())
+class PSLbot(Plugin):
+    @Plugin.listen('standings')
+    def getStandings(self, event):
+        html_link = "http://www.espncricinfo.com/table/series/8679/season/2018/pakistan-sl"
+        pslStandings = standings.Standing(html_link)
+        event.msg.reply(pslStandings.getStandings())
